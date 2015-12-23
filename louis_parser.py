f880cadbdb968879ae01e5811385ebb1b367983e
user <user@lorbanery>
2015-12-23 19:20:58 +0000

    More

--- louis_parser.py
+++ louis_parser.py
@@ -1,15 +1,30 @@
 import logging
 from collections import defaultdict, namedtuple
 from pprint import pprint
+import sys
 
-TABLE='/usr/share/liblouis/tables/en-GB-g2.ctb'
+# TABLE='/usr/share/liblouis/tables/en-GB-g2.ctb'
+TABLE='/usr/share/liblouis/tables/en-gb-g1.utb'
 
 # http://liblouis.org/documentation/liblouis.html
 
-Pattern = namedtuple('Pattern', 'table word comment')
+Word = namedtuple('Word', 'table pattern abc_word comment')
+
+WORDS = {'always', 'begword', 'word', 'midendword', 'joinword', 'sufword', 'lowword', 'midword', 'endword', 'endnum', 'largesign', 'begnum', 'sign', 'litdigit', 'joinnum', 'midnum', 'uplow', 'prepunc', 'postpunc', 'decpoint', 'multind', 'letter', 'hyphen', 'punctuation'}
+
+FORMATTING = {'begital', 'endital', 'begbold', 'endbold', 'begcaps', 'endcaps', 'capsign', 'numsign', 'letsign', 'begcomp', 'endcomp'}
+
+IGNORE = {'literal', 'include', 'repeated', 'contraction'}
+
+class DotLetter(object):
+	def __init__(self):
+		self.things = {w : [] for w in WORDS | FORMATTING}
+	def __repr__(self):
+		return 'DotLetter(%r)' % (self.__dict__,)
 
 def parse_table(table):
-	dot_words = defaultdict(list)
+	dot_letters = defaultdict(DotLetter)
+
 	with open(table) as f:
 		for line in f:
 			tokens = line.strip().split()
@@ -19,29 +34,42 @@ def parse_table(table):
 			if tokens[0].startswith('#'):
 				continue
 
-			if tokens[0] == 'always':
-				word = tokens[1]
-				if tokens[2] == '=':
-					pass # use the same dot pattern as last time
+			if tokens[0] in IGNORE:
+				continue
+
+			elif tokens[0] in FORMATTING:
+				abc_word = tokens[1]
+				if tokens[2:]:
+					comment = 'For example, in %s' % (', '.join(tokens[2:]))
 				else:
-					dot = tokens[2]
+					comment = ''
+				dot_word = ''
+
+				word = Word(table, dot_word, abc_word, comment)
+				for dot in split_dot_word(dot_word):
+					dot_letters[dot].things[tokens[0]].append(word)
+
+			elif tokens[0] in WORDS:
+				abc_word = tokens[1]
+				if tokens[2] == '=':
+					pass # use the same dot_word pattern as last time
+				else:
+					dot_word = tokens[2]
 				if tokens[3:]:
 					comment = 'For example, in %s' % (', '.join(tokens[3:]))
 				else:
 					comment = ''
-				dot_words[dot].append(Pattern(table, word, comment))
+
+				word = Word(table, dot_word, abc_word, comment)
+				for dot in split_dot_word(dot_word):
+					dot_letters[dot].things[tokens[0]].append(word)
 			else:
 				logging.warn('Unknown token %r', tokens)
-	pprint(dict(
-		letter_patterns(dot_words)
-	))
 
-def letter_patterns(dot_words):
-	char_lookup = defaultdict(dict)
-	for dot_word, patterns in dot_words.items():
-		for dot in dot_word.split('-'):
-			char_lookup[dot][dot_word] = patterns
-	return char_lookup
+	pprint(dot_letters[sys.argv[1]].__dict__)
+
+def split_dot_word(dot_word):
+	return dot_word.split('-')
 
 def main():
 	logging.getLogger().setLevel(logging.INFO)
